e75cb6aab54d0d809b184ccf9548b2630dc70379
pycyyaxyy <pycyyaxyy@gmail.com>
2022-03-04 19:48:19 +0800

    修补查不到动态配图的bug—主机名和端口号写死了

--- src/service/moment.service.js
+++ src/service/moment.service.js
@@ -1,4 +1,5 @@
 const connection = require("../app/database");
+const { APP_HOST, APP_PORT } = require("../app/config");
 
 class MomentService {
   async create(userId, content) {
@@ -30,7 +31,7 @@ class MomentService {
           JSON_OBJECT('id', c.id, 'content', c.content, 'commentId', c.comment_id, 'createTime', c.createAt,
                       'user', JSON_OBJECT('id', cu.id, 'name', cu.name, 'avatarUrl', cu.avatar_url))
         ),NULL) FROM comment c LEFT JOIN user cu ON c.user_id = cu.id WHERE m.id = c.moment_id) comments,
-        (SELECT JSON_ARRAYAGG(CONCAT('http://localhost:8000/moment/images/', file.filename))
+        (SELECT JSON_ARRAYAGG(CONCAT('${APP_HOST}:${APP_PORT}/moment/images/', file.filename))
         FROM file WHERE m.id = file.moment_id) images
       FROM moment m
       LEFT JOIN user u ON m.user_id = u.id
@@ -55,7 +56,7 @@ class MomentService {
          JSON_OBJECT('id', u.id, 'name', u.name ,'avatarUrl', u.avatar_url) author,
          (SELECT COUNT(*) FROM comment c WHERE c.moment_id = m.id) commentCount,
          (SELECT COUNT(*) FROM moment_label ml WHERE ml.moment_id = m.id) labelCount,
-         (SELECT JSON_ARRAYAGG(CONCAT('http://localhost:8000/moment/images/', file.filename)) 
+         (SELECT JSON_ARRAYAGG(CONCAT('${APP_HOST}:${APP_PORT}/moment/images/', file.filename)) 
          FROM file WHERE m.id = file.moment_id) images
          FROM moment m
      LEFT JOIN user u ON m.user_id = u.id
@@ -102,7 +103,7 @@ class MomentService {
           JSON_OBJECT('id', c.id, 'content', c.content, 'commentId', c.comment_id, 'createTime', c.createAt,
                       'user', JSON_OBJECT('id', cu.id, 'name', cu.name, 'avatarUrl', cu.avatar_url))
         ),NULL) FROM comment c LEFT JOIN user cu ON c.user_id = cu.id WHERE m.id = c.moment_id) comments,
-        (SELECT JSON_ARRAYAGG(CONCAT('http://localhost:8000/moment/images/', file.filename))
+        (SELECT JSON_ARRAYAGG(CONCAT('${APP_HOST}:${APP_PORT}/moment/images/', file.filename))
         FROM file WHERE m.id = file.moment_id) images
       FROM moment m
       LEFT JOIN user u ON m.user_id = u.id
